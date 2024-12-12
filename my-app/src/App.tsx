50cc4a5d0d4f2d4555ed0009dedfa28513be56fc
Mixail <ssmitss58@gmail.com>
2024-12-12 20:40:37 +0300

    chenge componentn regustration as AuthForm in app tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,11 +3,11 @@ import { History } from './pages/history/history';
 import { Favourites } from './pages/favourites/Favorurites';
 import { Characters } from './pages/characters/Characters';
 import { NotFound } from './pages/notFound/NotFound';
-<<<<<<< HEAD
+
 import { AuthForm } from './components/Ui/AuthForm/AuthForm';
-=======
-import { Registration } from './pages/registration/Registration';
->>>>>>> 629fd6729f634eee6e898908ef690088429706eb
+
+
+
 import './App.css';
 import { Layout } from './components/Layout/Layout';
 
@@ -17,15 +17,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Characters/>}/>
-<<<<<<< HEAD
-          <Route path='/locations' element={<Locations/>}/>
-          <Route path='/episodes' element={<Episodes/>}/>
-          <Route path='/authorization' element={<AuthForm/>}/>
-=======
+
+
           <Route path='/favourites' element={<Favourites/>}/>
           <Route path='/history' element={<History/>}/>
-          <Route path='/registration' element={<Registration/>}/>
->>>>>>> 629fd6729f634eee6e898908ef690088429706eb
+       
+          <Route path='/registration' element={<AuthForm/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Route>
       </Routes>
--- my-app/src/pages/registration/Registration.tsx
+++ my-app/src/pages/registration/Registration.tsx
@@ -5,14 +5,5 @@ const Authorization = () => {
     return <AuthForm />
 };
 
-<<<<<<< HEAD:my-app/src/pages/authorization/Authorization.tsx
+
 export default Authorization;
-=======
-export function Registration(){
-    return(
-        <>
-         <Form/>
-        </>
-    )
-}
->>>>>>> 629fd6729f634eee6e898908ef690088429706eb:my-app/src/pages/registration/Registration.tsx
